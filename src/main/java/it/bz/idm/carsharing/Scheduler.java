d65c25f85837c79696a36a7768a493d09c54852e
sdalvai <simon.dalvai@idm-suedtirol.com>
2017-01-25 15:20:15 +0100

    fixed time for scheduled tasks. configured logger

--- src/main/java/it/bz/idm/carsharing/Scheduler.java
+++ src/main/java/it/bz/idm/carsharing/Scheduler.java
@@ -1,19 +1,22 @@
 package it.bz.idm.carsharing;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
-	
-	
-	 @Scheduled(fixedRate = 5000)
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Scheduled(fixedRate = 600000) // 10 minutes interval
 	public void staticTask() {
-		System.err.println("Static Task");
+		logger.info("Static Task");
 	}
-	
-	 @Scheduled(cron="0 15 10 * * *")
+
+	@Scheduled(cron = "0 0 0 * * *") // every day at midnight
 	public void realTimeTask() {
-		System.err.println("Real Time Task");
+		logger.info("Real Time Task");
 	}
 }
