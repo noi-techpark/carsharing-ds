43a8f9d07fe003a3978f8bc5df18d39788358cf2
sdalvai <simon.dalvai@idm-suedtirol.com>
2017-03-27 09:43:35 +0200

    checked in RestTemplateErrorHandler

---
+++ src/main/java/it/bz/idm/carsharing/RestTemplateErrorHandler.java
@@ -0,0 +1,18 @@
+package it.bz.idm.carsharing;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+
+public class RestTemplateErrorHandler extends DefaultResponseErrorHandler {
+	private final Logger logger = LoggerFactory.getLogger(RestTemplateErrorHandler.class);
+
+	@Override
+	public void handleError(ClientHttpResponse response) throws IOException {
+		logger.error("RestTemplate Http Error with code: " + response.getRawStatusCode() + " and Status Text: "
+				+ response.getStatusText());
+	}
+}
